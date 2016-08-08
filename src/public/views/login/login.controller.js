d12bb8c56d2588ed50921153bf50b66ab1016e9d
jeremie <pro_rish@yahoo.com>
2016-08-08 18:24:33 +0200

    starting the login page

--- src/public/views/login/login.controller.js
+++ src/public/views/login/login.controller.js
@@ -3,8 +3,20 @@ angular
 	.controller('LoginCtrl', LoginCtrl);
 
 function LoginCtrl($scope) {
-	const vm = this;
+	var utilisateursDatabase = [{'login':'ngcode','password':'tutoriel'}];
 
-
-
+    $scope.connexion = function(){
+        var connected = false;
+        for(var i in utilisateursDatabase){
+            var utilisateur = utilisateursDatabase[i];
+            if(_.isEqual(utilisateur,$scope.identifiants)){
+                connected = true;
+            }
+        }
+        if(connected){
+            $scope.resultat = "Connexion OK";
+        }else{
+            $scope.resultat = "Connexion KO";
+        }
+    }
 }
