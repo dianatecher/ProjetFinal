ed282b87b8be451077217e5b7c2cb5dd05f57b6f
WOLFSilverWolf <ginnefirimu@gmail.com>
2016-08-10 18:36:00 +0200

    Blame Yoann for the link sass in index.html.

--- src/public/blog.config.js
+++ src/public/blog.config.js
@@ -4,22 +4,22 @@ import style from './style.sass';
 
 angular
 	.module('blog')
-	.config(function($routeProvider){
-	$routeProvider
-	.when('/', {
-		templateUrl: './views/home.html',
-		controller: 'HomeCtrl'
-	})
-	.when('/login', {
-		templateUrl: './views/login.html',
-		controller: 'LoginCtrl'
-	})
-	.when('/signup', {
-		templateUrl: './views/signup.html',
-		controller: 'SignupCtrl'
-	})
-	.when('/admin', {
-		templateUrl: './views/admin.html',
-		controller: 'AdminCtrl'
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: './views/home.html',
+				controller: 'HomeCtrl'
+			})
+			.when('/login', {
+				templateUrl: './views/login.html',
+				controller: 'LoginCtrl'
+			})
+			.when('/signup', {
+				templateUrl: './views/signup.html',
+				controller: 'SignupCtrl'
+			})
+			.when('/admin', {
+				templateUrl: './views/admin.html',
+				controller: 'AdminCtrl'
+			});
 	});
-});
