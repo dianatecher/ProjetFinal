b030315b707f01ff4ab762a74be89da271ffac62
Shinji <shinji51@hotmail.fr>
2016-08-10 16:39:03 +0200

    ajouter d'un objet js pour simuler une BD et faire des test de vue avec angular

--- src/public/views/home/home.controller.js
+++ src/public/views/home/home.controller.js
@@ -3,4 +3,18 @@ angular
 	.controller('HomeCtrl', HomeCtrl);
 
 function HomeCtrl() {
+	const vm = this;
+
+	const article = {
+		img: "",
+		titre: "Mort lors d'un barbecue près d'un volcan",
+		time: "Il y a 10 min",
+		contenu: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa iusto ut incidunt, nobis soluta culpa illum autem."
+	};
+
+	vm.product = article;
+
 }
+
+
+
